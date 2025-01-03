8043350d58bf356721ff0cef28d1062b96717557
VimalWill <vimalwilliam99@gmail.com>
2025-01-04 00:32:27 +0530

    custom backend for qpu

--- codegen/compiler/src/QPU/Target/QpuTarget.cpp
+++ codegen/compiler/src/QPU/Target/QpuTarget.cpp
@@ -1,1 +1,7 @@
 #include "iree/compiler/PluginAPI/Client.h"
+#include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenDialect.h"
+#include "iree/compiler/Dialect/HAL/Target/TargetRegistry.h"
+
+class QpuTargetBackend final : public IREE::HAL::TargetBackend {
+    // TODO: custom backend implementation
+}; 
