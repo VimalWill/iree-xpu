30ed9f2d6225f642688b9cd83356d6539342c364
VimalWill <vimalwilliam99@gmail.com>
2025-01-04 15:33:16 +0530

    trying to understand HAL::ExecutableTargetAttr

--- codegen/compiler/src/QPU/Target/QpuTarget.cpp
+++ codegen/compiler/src/QPU/Target/QpuTarget.cpp
@@ -1,32 +1,41 @@
-#include "iree/compiler/PluginAPI/Client.h"
 #include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenDialect.h"
 #include "iree/compiler/Dialect/HAL/Target/TargetRegistry.h"
+#include "iree/compiler/PluginAPI/Client.h"
 #include "mlir/Pass/PassManager.h"
 
+#include "QPU/Target/TargetUtils.cpp"
 
 #include <string>
 
-using namespace mlir; 
-using namespace mlir::iree_compiler; 
-struct QPUTargetOptions {
+using namespace mlir;
+using namespace mlir::iree_compiler;
 
-}; 
+struct QPUTargetOptions {};
+
 class QPUTargetBackend final : public IREE::HAL::TargetBackend {
 public:
-    explicit QPUTargetBackend(QPUTargetOptions options)
-        : options(std::move(options)) {}
-    
-    std::string getLegacyDefaultDeviceID() const override { return "qpu";}
+  explicit QPUTargetBackend(QPUTargetOptions options)
+      : options_(std::move(options)) {}
 
-    void getDefaultExecutableTargets(
-        MLIRContext *context, StringRef deviceID, DictionaryAttr deviceConfigAttr, 
-        SmallVectorImpl<IREE::HAL::ExecutableTargetAttr> &executableTargetAttrs
-    ) const override {
-        // todo: hardware data-layout
-    }
+  std::string getLegacyDefaultDeviceID() const override { return "qpu-device"; }
 
-    
+  void getDefaultExecutableTargets(
+      MLIRContext *context, StringRef deviceID, DictionaryAttr deviceConfigAttr,
+      SmallVectorImpl<IREE::HALL::ExecutableTargetAttr> &executableTargetAttrs)
+      const override {
+    executableTargetAttrs.push_back(getExecutableTarget(context));
+  }
+
+  IREE::HAL::ExecutableTargetAttr
+  getExecutableTarget(MLIRContext *context) const {
+    Builder b(context);
+    SmallVector<NamedAttribute> configItems;
+
+    auto addConfig = [&](StringRef name, Attribute value) {
+      configItems.emplace_back(StringAttr::get(context, name), value);
+    };
+  }
 
 private:
-    QPUTargetOptions options;
-}; 
+  QPUTargetOptions options_;
+};
