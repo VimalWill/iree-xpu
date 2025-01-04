26c58242355422cda51c94f6337a185e4cc573c1
VimalWill <vimalwilliam99@gmail.com>
2025-01-04 12:41:14 +0530

    should thing about hardware details

--- codegen/compiler/src/QPU/Target/QpuTarget.cpp
+++ codegen/compiler/src/QPU/Target/QpuTarget.cpp
@@ -1,7 +1,32 @@
 #include "iree/compiler/PluginAPI/Client.h"
 #include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenDialect.h"
 #include "iree/compiler/Dialect/HAL/Target/TargetRegistry.h"
+#include "mlir/Pass/PassManager.h"
 
-class QpuTargetBackend final : public IREE::HAL::TargetBackend {
-    // TODO: custom backend implementation
+
+#include <string>
+
+using namespace mlir; 
+using namespace mlir::iree_compiler; 
+struct QPUTargetOptions {
+
+}; 
+class QPUTargetBackend final : public IREE::HAL::TargetBackend {
+public:
+    explicit QPUTargetBackend(QPUTargetOptions options)
+        : options(std::move(options)) {}
+    
+    std::string getLegacyDefaultDeviceID() const override { return "qpu";}
+
+    void getDefaultExecutableTargets(
+        MLIRContext *context, StringRef deviceID, DictionaryAttr deviceConfigAttr, 
+        SmallVectorImpl<IREE::HAL::ExecutableTargetAttr> &executableTargetAttrs
+    ) const override {
+        // todo: hardware data-layout
+    }
+
+    
+
+private:
+    QPUTargetOptions options;
 }; 
