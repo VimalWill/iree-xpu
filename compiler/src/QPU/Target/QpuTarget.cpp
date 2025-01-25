930d5c02e73e12d7a03c97ab5cd4152eadbae147
VimalWill <vimalwilliam99@gmail.com>
2025-01-25 20:41:03 +0530

    walk through func to find matmulop

--- compiler/src/QPU/Target/QpuTarget.cpp
+++ compiler/src/QPU/Target/QpuTarget.cpp
@@ -1,11 +1,17 @@
 #include "iree/compiler/Codegen/Dialect/Codegen/IR/IREECodegenDialect.h"
 #include "iree/compiler/Dialect/HAL/Target/TargetRegistry.h"
+#include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtDialect.h"
 #include "iree/compiler/PluginAPI/Client.h"
 #include "mlir/Pass/PassManager.h"
 #include "llvm/Support/SourceMgr.h"
 
+#include "mlir/IR/DialectRegistry.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllExtensions.h"
+
 #include "QPU/Target/TargetUtils.cpp"
 
+#include <cstdint>
 #include <string>
 
 using namespace mlir;
@@ -33,19 +39,37 @@ public:
   getExecutableTarget(MLIRContext *context) const {
     Builder b(context);
     SmallVector<NamedAttribute> configItems;
+
+    uint32_t numPE = 4;
+    uint32_t numCl = 8;
+
     auto addConfig = [&](StringRef name, Attribute value) {
-      configItems.emplace_back(StringAttr::get(context, name), value);
+      configItems.emplace_back(StringAttr::get(context, value), value);
     };
 
-    if (DeviceModel target = QPU::getDeviceModel(options_.target, context)) {
-      addConfig("", target);
-    } else {
-      emitError(b.getUnknownLoc(), "Unknown Target");
-      return nullptr;
-    }
+    addConfig("target_device", StringAttr::get(context, "qpu_v1"));
+    addConfig("num_pe", IntegerAttr::get(IntegerType::get(context, 32), numPE));
+    addConfig("num_cl", IntegerAttr::get(IntegerType::get(context, 32), numCl));
+    auto configAttr = b.getDictionaryAttr(configItems);
+    return IREE::HAL::ExecutableTargetAttr::get(
+        context, b.getStringAttr("qpu"), b.getStringAttr("qpu-fb"), configAttr);
+  }
+
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<mlir::iree_compiler::QPU::QPUDialect,
+                    mlir::iree_compiler::IREE::Codegen::IREECodegenDialect,
+                    IREE::LinalgExt::IREELinalgExtDialect>();
+
+    mlir::registerBuiltinDialectTranslation(registry); // may break?!
+  }
+
+  void buildTranslationPassPipeline(IREE::HAL::ExecutableTargetAttr,
+                                    OpPassManager &passManager) override {
+
+    // todo: conversion LinalgExt -> QPUDialect
   }
 
 private:
   QPUTargetOptions options_;
 };
-} //namespace
+} // namespace mlir::iree_compiler::QPU
